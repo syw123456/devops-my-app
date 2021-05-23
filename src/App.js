2045e6a74aeac2699264d352b8404384d1c6b322
ArianShi <arianandcode@gmail.com>
2021-05-23 13:27:41 +0800

    feat: init project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 232232323
         </a>
       </header>
     </div>
