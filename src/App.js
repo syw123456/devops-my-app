eeb4cab33e2c2a4fe801c4ba375db940b3bcb7f3
ArianShi <arianandcode@gmail.com>
2021-05-26 14:50:06 +0800

    feat: updated content

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+         >
+          Learn React Arian
         </a>
       </header>
     </div>
